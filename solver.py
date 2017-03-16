220c4271a084255175ad3d20e78fb6942b7b1603
Wail Khemir (wkhemir) <wkhemir@cisco.com>
2017-03-16 12:05:38 -0400

    Added gradient descent

--- solver.py
+++ solver.py
@@ -14,4 +14,19 @@ def newton(f, x, y, e=0.0001, verbose=False):
             break
 
         cost_old = cost_new
+
+def gradient_descent(f, x, y, alpha=0.0001, e=0.01, verbose=False):
+    cost_old = float("inf")
+    cost_new = float("inf")
+    
+    while True:
+        f.w = f.w - alpha * f.gradient(x, y)
+
+        cost_new = f.cost(x, y)
+        if verbose:
+            print("cost: %f" % cost_new)
+        if abs(cost_new - cost_old) < e:
+            break
+
+        cost_old = cost_new
          
